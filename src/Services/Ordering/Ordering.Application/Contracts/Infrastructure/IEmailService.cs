79865fea64b70439c21831762c93d189cfc31040
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-09-06 12:26:27 -0400

    Added Email Related classes

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
